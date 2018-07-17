71158abfff016ccad290ccc1f31ae0dcc514cb48
sophiahusak <72futbolplayer@gmail.com>
2018-07-17 19:50:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,5 +19,9 @@ get '/say/:number/:phrase' do
 end
 
 get '/say/:word1/:word2/:word3/:word4/:word5' do 
-  
+  @word1 = params[:word1].to_i
+  @word2 = params[:word2].to_i
+  @word3 = params[:word3].to_i
+  @word4 = params[:word4].to_i
+  @word5 = params[:word5].to_i
 end
